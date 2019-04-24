9f81c50077997190a80b3a3e05e74d1ebcfeb14a
Kohei Asai <me@axross.io>
2019-04-24 09:57:07 -0700

    Task 3

--- 02-01/app/src/main/java/app/axross/ciccc/twoactivities/MainActivity.java
+++ 02-01/app/src/main/java/app/axross/ciccc/twoactivities/MainActivity.java
@@ -5,20 +5,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private static String LOG_TAG = MainActivity.class.getSimpleName();
+
+    public static final String EXTRA_MESSAGE =
+            "app.axross.ciccc.twoactivities.extra.MESSAGE";
+
+    private EditText mMessageEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMessageEditText = findViewById(R.id.editText_main);
     }
 
     public void launchSecondActivity(View view) {
         Log.d(LOG_TAG, "Button Clicked!");
 
+        String message = mMessageEditText.getText().toString();
+
         Intent intent = new Intent(this, SecondActivity.class);
+
+        intent.putExtra(EXTRA_MESSAGE, message);
 
         startActivity(intent);
     }
