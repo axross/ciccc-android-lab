3c081d52c10a0e907c96e95046dfcb3385bf9a51
Kohei Asai <me@axross.io>
2019-04-26 09:11:11 -0700

    Copy a project from 02-01

---
+++ 02-02/app/src/main/java/app/axross/ciccc/twoactivities/MainActivity.java
@@ -0,0 +1,76 @@
+package app.axross.ciccc.twoactivities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE =
+            "app.axross.ciccc.twoactivities.extra.MESSAGE";
+    public static final String EXTRA_PASSAGE =
+            "app.axross.ciccc.twoactivities.extra.PASSAGE";
+    public static final int TEXT_REQUEST = 1;
+    private static String LOG_TAG = MainActivity.class.getSimpleName();
+    private TextView mReplyHeadTextView;
+    private TextView mReplyTextView;
+    private EditText mMessageEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mReplyHeadTextView = findViewById(R.id.text_header_reply);
+        mReplyTextView = findViewById(R.id.text_message_reply);
+        mMessageEditText = findViewById(R.id.editText_main);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == TEXT_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                String reply = data.getStringExtra(SecondActivity.EXTRA_REPLY);
+
+                mReplyHeadTextView.setVisibility(View.VISIBLE);
+                mReplyTextView.setVisibility(View.VISIBLE);
+                mReplyTextView.setText(reply);
+            }
+        }
+    }
+
+    public void launchSecondActivity(View view) {
+        Log.d(LOG_TAG, "Button Clicked!");
+
+        String message = mMessageEditText.getText().toString();
+
+        Intent intent = new Intent(this, SecondActivity.class);
+
+        intent.putExtra(EXTRA_MESSAGE, message);
+
+        startActivityForResult(intent, TEXT_REQUEST);
+    }
+
+    public void onClickAnyPassageButton(View view) {
+        Intent intent = new Intent(this, PassageActivity.class);
+
+        switch(view.getId()) {
+            case R.id.button_one:
+                intent.putExtra(EXTRA_PASSAGE, 0);
+                break;
+            case R.id.button_two:
+                intent.putExtra(EXTRA_PASSAGE, 1);
+                break;
+            case R.id.button_three:
+                intent.putExtra(EXTRA_PASSAGE, 2);
+                break;
+        }
+
+        startActivity(intent);
+    }
+}
