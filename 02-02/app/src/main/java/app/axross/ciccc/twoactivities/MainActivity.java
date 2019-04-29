d80c994e26547c758e202d6fc66ffc340395d6f3
Kohei Asai <me@axross.io>
2019-04-28 18:02:59 -0700

    Lab 02.2 (#3)
    * Copy a project from 02-01
    
    * Task 1
    
    * Task 2
    
    * Coding challenge

---
+++ 02-02/app/src/main/java/app/axross/ciccc/twoactivities/MainActivity.java
@@ -0,0 +1,153 @@
+package app.axross.ciccc.twoactivities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE =
+            "app.axross.ciccc.twoactivities.extra.MESSAGE";
+    public static final String EXTRA_PASSAGE =
+            "app.axross.ciccc.twoactivities.extra.PASSAGE";
+    public static final int TEXT_REQUEST = 1;
+    private static String LOG_TAG = MainActivity.class.getSimpleName();
+    private TextView mReplyHeadTextView;
+    private TextView mReplyTextView;
+    private EditText mMessageEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Log.d(LOG_TAG, "-------");
+        Log.d(LOG_TAG, "onCreate");
+
+        setContentView(R.layout.activity_main);
+
+        mReplyHeadTextView = findViewById(R.id.text_header_reply);
+        mReplyTextView = findViewById(R.id.text_message_reply);
+        mMessageEditText = findViewById(R.id.editText_main);
+
+        Log.d(LOG_TAG, String.format("savedInstanceState: %s", savedInstanceState));
+
+        if (savedInstanceState != null) {
+            boolean isVisible = savedInstanceState.getBoolean("reply_visible");
+
+            if (isVisible) {
+                mReplyHeadTextView.setVisibility(View.VISIBLE);
+                mReplyTextView.setText(savedInstanceState.getString("reply_text"));
+                mReplyTextView.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.d(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.d(LOG_TAG, "onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.d(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        Log.d(LOG_TAG, "onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.d(LOG_TAG, "onDestroy");
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == TEXT_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                String reply = data.getStringExtra(SecondActivity.EXTRA_REPLY);
+
+                mReplyHeadTextView.setVisibility(View.VISIBLE);
+                mReplyTextView.setVisibility(View.VISIBLE);
+                mReplyTextView.setText(reply);
+            }
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Log.d(LOG_TAG, outState.toString());
+
+        if (mReplyHeadTextView.getVisibility() == View.VISIBLE) {
+            outState.putBoolean("reply_visible", true);
+        }
+
+        outState.putString("reply_text",mReplyTextView.getText().toString());
+    }
+
+    public void launchSecondActivity(View view) {
+        Log.d(LOG_TAG, "Button Clicked!");
+
+        String message = mMessageEditText.getText().toString();
+
+        Intent intent = new Intent(this, SecondActivity.class);
+
+        intent.putExtra(EXTRA_MESSAGE, message);
+
+        startActivityForResult(intent, TEXT_REQUEST);
+    }
+
+    public void onClickAnyPassageButton(View view) {
+        Intent intent = new Intent(this, PassageActivity.class);
+
+        switch(view.getId()) {
+            case R.id.button_one:
+                intent.putExtra(EXTRA_PASSAGE, 0);
+                break;
+            case R.id.button_two:
+                intent.putExtra(EXTRA_PASSAGE, 1);
+                break;
+            case R.id.button_three:
+                intent.putExtra(EXTRA_PASSAGE, 2);
+                break;
+        }
+
+        startActivity(intent);
+    }
+
+    public void onClickShoppingButton(View view) {
+        Intent intent = new Intent(this, ShoppingMainActivity.class);
+
+        startActivity(intent);
+    }
+}
