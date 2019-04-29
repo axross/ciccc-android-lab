b84fe9a8c13cf6d504601eb4e878bf7823df96df
Kohei Asai <me@axross.io>
2019-04-29 09:30:43 -0700

    Task 1

---
+++ 02-03/app/src/main/java/app/axross/ciccc/implicitintents/MainActivity.java
@@ -0,0 +1,23 @@
+package app.axross.ciccc.implicitintents;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onWebsiteButtonClick(View view) {
+    }
+
+    public void onLocationButtonClick(View view) {
+    }
+
+    public void onShareButtonClick(View view) {
+    }
+}
