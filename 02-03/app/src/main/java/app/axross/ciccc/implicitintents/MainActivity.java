5a0baf07539df798a2e93749746ce29ae068c809
Kohei Asai <me@axross.io>
2019-04-29 09:48:35 -0700

    Task 2

--- 02-03/app/src/main/java/app/axross/ciccc/implicitintents/MainActivity.java
+++ 02-03/app/src/main/java/app/axross/ciccc/implicitintents/MainActivity.java
@@ -1,21 +1,42 @@
 package app.axross.ciccc.implicitintents;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText websiteEditText;
+    private EditText locationEditText;
+    private EditText shareEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        websiteEditText = findViewById(R.id.edittext_website);
+        locationEditText = findViewById(R.id.edittext_location);
+        shareEditText = findViewById(R.id.edittext_share);
     }
 
     public void onWebsiteButtonClick(View view) {
+        String urlString = websiteEditText.getText().toString();
+        Uri url = Uri.parse(urlString);
+        Intent intent = new Intent(Intent.ACTION_VIEW, url);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this intent!");
+        }
     }
 
     public void onLocationButtonClick(View view) {
+
     }
 
     public void onShareButtonClick(View view) {
