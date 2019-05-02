84dff81830cf98315481919e41ced038ca5b7fe7
Kohei Asai <me@axross.io>
2019-05-01 20:15:17 -0700

    Task 2

--- SimpleCalc/app/src/test/java/app/axross/ciccc/simplecalc/CalculatorTest.java
+++ SimpleCalc/app/src/test/java/app/axross/ciccc/simplecalc/CalculatorTest.java
@@ -5,8 +5,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.Matchers.closeTo;
 import static org.junit.Assert.assertThat;
 
 /**
@@ -34,5 +34,54 @@ public class CalculatorTest {
         double result = calculator.add(1d, 1d);
 
         assertThat(result, is(equalTo(2d)));
+    }
+
+    @Test
+    public void addTwoNegativeNumbers() {
+        double result = calculator.add(-1d, 2d);
+
+        assertThat(result, is(equalTo(1d)));
+    }
+
+    @Test
+    public void addTwoFloatNumbers() {
+        double result = calculator.add(1.111f, 1.111d);
+
+        assertThat(result, is(closeTo(2.222, 0.01)));
+    }
+
+    @Test
+    public void subTwoNumbers() {
+        double resultSub = calculator.sub(1d, 1d);
+
+        assertThat(resultSub, is(equalTo(0d)));
+    }
+
+    @Test
+    public void subWorksWithNegativeResult() {
+        double resultSub = calculator.sub(1d, 17d);
+
+        assertThat(resultSub, is(equalTo(-16d)));
+    }
+
+    @Test
+    public void mulTwoNumbers() {
+        double resultMul = calculator.mul(32d, 2d);
+
+        assertThat(resultMul, is(equalTo(64d)));
+    }
+
+    @Test
+    public void divTwoNumbers() {
+        double resultDiv = calculator.div(32d,2d);
+
+        assertThat(resultDiv, is(equalTo(16d)));
+    }
+
+    @Test
+    public void divTwoNumbersZero() {
+        double resultDiv = calculator.div(32d,0);
+
+        assertThat(resultDiv, is(equalTo(Double.POSITIVE_INFINITY)));
     }
 }
